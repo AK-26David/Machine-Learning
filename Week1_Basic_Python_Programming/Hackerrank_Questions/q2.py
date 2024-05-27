777438082e6100e1e37e20cf0d78946150025371
AK-26David <arnav.karnik@gmail.com>
2024-05-27 23:32:17 +0530

    q2

---
+++ Week1_Basic_Python_Programming/Hackerrank_Questions/q2.py
@@ -0,0 +1,19 @@
+#Task
+#The provided code stub reads two integers from STDIN, and
+#. Add code to print three lines where:
+# The first line contains the sum of the two numbers.
+#The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+def main():
+    s=a+b
+    d=a-b
+    p=a*b
+    print(s)
+    print(d)
+    print(p)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    main()
