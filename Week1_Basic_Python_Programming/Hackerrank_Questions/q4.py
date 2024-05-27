41d467ac84a4732baaeacd5f99ec9e7019ef15fe
AK-26David <arnav.karnik@gmail.com>
2024-05-27 23:42:57 +0530

    q4

---
+++ Week1_Basic_Python_Programming/Hackerrank_Questions/q4.py
@@ -0,0 +1,12 @@
+#Task
+#The provided code stub reads and integer,n , from STDIN. For all non-negative integers , print.
+#Example
+#n=3
+#The list of non-negative integers that are less than is 0,1,2. Print the square of each number on a separate line.
+
+def main():
+    for i in range(0,n):
+        print(i*i)
+if __name__ == '__main__':
+    n = int(input())
+    main()
